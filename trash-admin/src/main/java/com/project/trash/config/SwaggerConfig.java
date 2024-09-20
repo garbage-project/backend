4bff5a133b8dc487285b49b52ca9aa483b48af56
byung0216 <byung0216@gmail.com>
2024-09-21 00:10:57 +0900

    [FIX] swagger server url 설정

--- trash-admin/src/main/java/com/project/trash/config/SwaggerConfig.java
+++ trash-admin/src/main/java/com/project/trash/config/SwaggerConfig.java
@@ -1,20 +1,31 @@
 package com.project.trash.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
 
+  @Value("${springdoc.swagger-ui.server-url}")
+  private String serverUrl;
+
   @Bean
   public OpenAPI openAPI() {
+    Server server = new Server();
+    server.setUrl(serverUrl);
+
     return new OpenAPI()
         .components(new Components())
-        .info(apiInfo());
+        .info(apiInfo())
+        .servers(List.of(server));
   }
 
   private Info apiInfo() {
